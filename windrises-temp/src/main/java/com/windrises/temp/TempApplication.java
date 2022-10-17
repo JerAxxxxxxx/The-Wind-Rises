62f7f72d76e3a520eb5da4cbd8046071ddba991c
JerAxxxxxxx <q14341319@gmail.com>
2022-10-17 13:33:37 +0800

    chore:update

---
+++ windrises-temp/src/main/java/com/windrises/temp/TempApplication.java
@@ -0,0 +1,16 @@
+package com.windrises.temp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JerAxxxxx
+ * @version Revision 1.0.0
+ * @date 2020/4/27 10:30
+ */
+@SpringBootApplication
+public class TempApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TempApplication.class, args);
+    }
+}
