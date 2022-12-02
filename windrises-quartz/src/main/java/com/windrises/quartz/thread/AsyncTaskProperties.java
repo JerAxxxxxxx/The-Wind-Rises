d483c7d41b7b08c5543497c8e701858031bb8368
JerAxxxxxxx <q14341319@gmail.com>
2022-12-02 16:31:58 +0800

    add validation

--- windrises-quartz/src/main/java/com/windrises/quartz/thread/AsyncTaskProperties.java
+++ windrises-quartz/src/main/java/com/windrises/quartz/thread/AsyncTaskProperties.java
@@ -1,8 +1,12 @@
 package com.windrises.quartz.thread;
 
-import lombok.Data;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Data;
 
 /**
  * 线程池配置属性类
@@ -13,25 +17,30 @@ import org.springframework.stereotype.Component;
  */
 @Data
 @Component
+@Validated
 @ConfigurationProperties(prefix = "task.pool")
 public class AsyncTaskProperties {
     /**
      * 核心线程数
      */
+    @NotNull(message = "核心线程数不能为空")
     private int corePoolSize;
 
     /**
      * 最大线程数
      */
+    @NotNull(message = "最大线程数不能为空")
     private int maxPoolSize;
 
     /**
      * 活跃时间
      */
+    @NotNull(message = "活跃时间不能为空")
     private int keepAliveSeconds;
 
     /**
      * 队列容量
      */
+    @NotNull(message = "队列容量不能为空")
     private int queueCapacity;
 }
