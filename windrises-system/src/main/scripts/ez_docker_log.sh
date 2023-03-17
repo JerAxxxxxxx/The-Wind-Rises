271aca7db7bc9f45b88d01e901059b2d83d33846
JerAxxxxxxx <q14341319@gmail.com>
2023-03-17 10:37:07 +0800

    Update: 优化日志脚本

--- windrises-system/src/main/scripts/ez_docker_log.sh
+++ windrises-system/src/main/scripts/ez_docker_log.sh
@@ -6,7 +6,7 @@ function get_docker_log() {
   if [ -z "$1" ]; then
     echo "服务名为空!"
   else
-    echo "开始查询 $1 的 docker 日志"
+    echo "开始查询 $1 的 docker 日志, 读取行数为: ${BASE_LOG_LINE}"
     task_pid=$(docker ps | grep "$1" | awk '{print $1}')
     if [ -z "$task_pid" ]; then
       echo "找不到 $1 进程!"
--- windrises-system/src/main/scripts/ez_kubectl_log.sh
+++ windrises-system/src/main/scripts/ez_kubectl_log.sh
@@ -4,7 +4,7 @@ function get_kubectl_log() {
   if [ -z "$1" ]; then
     echo "服务名为空!"
   else
-    echo "开始查询 $1 的 kubectl 日志"
+    echo "开始查询 $1 的 kubectl 日志, 读取行数为: ${BASE_LOG_LINE}"
     task_pid=$(kubectl get pods -A | grep "$1" | awk '{print $2}')
     if [ -z "$task_pid" ]; then
       echo "找不到 $1 进程!"
